cb880ad64bd5ae24ba771ddea3b3056fdd55ba4a
Лефтеров Авраам Андреевич <avraamlefterov@yandex.ru>
2026-05-21 13:03:02 +0300

    robot

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,55 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Main {
+    public static final Map<Integer, Integer> sizeToFreq = new ConcurrentHashMap<>();
 
-    public static void main(String[] args) throws InterruptedException {
-        String[] texts = new String[25];
-        for (int i = 0; i < texts.length; i++) {
-            texts[i] = generateText("aab", 30_000);
-        }
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int numbersOfRoute = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+        List<Future<?>> futures = new LinkedList<>();
 
-        long startTs = System.currentTimeMillis(); // start time
+        long start = System.currentTimeMillis();
 
-        ExecutorService executor = Executors.newFixedThreadPool(4);
-        List <Future<Integer>> futures = new ArrayList<>();
+        for (int i = 0; i < numbersOfRoute; i++) {
+            futures.add(executorService.submit(() -> {
+                String route = generateRoute("RLRFR", 100);
+                int countR = route.length() - route.replace("R", "").length();
 
-        for (String text : texts) {
-            Future<Integer> future = executor.submit(() -> {
-                int maxSize = 0;
-                for (int i = 0; i < text.length(); i++) {
-                    for (int j = 0; j < text.length(); j++) {
-                        if (i >= j) {
-                            continue;
-                        }
-                        boolean bFound = false;
-                        for (int k = i; k < j; k++) {
-                            if (text.charAt(k) == 'b') {
-                                bFound = true;
-                                break;
-                            }
-                        }
-                        if (!bFound && maxSize < j - i) {
-                            maxSize = j - i;
-                        }
-                    }
+                synchronized (sizeToFreq) {
+                    sizeToFreq.merge(countR, 1, Integer::sum);
                 }
-                System.out.println(text.substring(0, 100) + " -> " + maxSize);
-                return maxSize;
-            });
-            futures.add(future);
+            }));
         }
+        for (Future<?> future : futures) {
+            future.get();
+        }
+        executorService.shutdown();
 
-        int globalMaxSize = 0;
-        for (Future<Integer> future : futures) {
-            int maxSize = 0;
-            try {
-                maxSize = future.get();
-            } catch (ExecutionException e) {
-                throw new RuntimeException(e);
-            }
-            if (maxSize > globalMaxSize) {
-                globalMaxSize = maxSize;
-            }
-        }
-        executor.shutdown();
-        long endTs = System.currentTimeMillis(); // end time
-        System.out.println("Общий максимальный интервал потоков: " + globalMaxSize);
-        System.out.println("Время: " + (endTs - startTs) + "мл/с");
+        int maxFreq = sizeToFreq.values().stream().max(Integer::compareTo).orElse(0);
+
+        int mostCommonSize = sizeToFreq.entrySet().stream()
+                .filter(e -> e.getValue() == maxFreq)
+                .findFirst().get().getKey();
+
+        System.out.println("Самое частое количество повторений " + mostCommonSize + " (встретилось " + maxFreq + " раз");
+        System.out.println("Другие размеры: ");
+
+        sizeToFreq.entrySet().stream()
+                .filter(e -> e.getKey() != mostCommonSize)
+                .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+                .forEach(e -> System.out.println("- " + e.getKey() + " (" + e.getValue() + " раз)"));
+
     }
-
-    public static String generateText(String letters, int length) {
+    public static String generateRoute(String letters, int length) {
         Random random = new Random();
-        StringBuilder text = new StringBuilder();
+        StringBuilder route = new StringBuilder();
         for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
+            route.append(letters.charAt(random.nextInt(letters.length())));
         }
-        return text.toString();
+        return route.toString();
     }
 }
